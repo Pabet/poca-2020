b241fb1d1b0bfb6d98d93dba9e00130afcd08946
Joackim Brigout Cavaud <joackim.brigout@gmail.com>
2020-10-11 17:00:26 +0200

    #23 implementation de User et Product

---
+++ src/main/scala/Products.scala
@@ -0,0 +1,20 @@
+package poca
+
+import scala.language.postfixOps
+import slick.jdbc.PostgresProfile.api._
+
+case class Product(productId: String,
+                    productName: String,
+                    productPrice: Double,
+                    productDetail: String)
+
+class Products {
+  class ProductsTable(tag: Tag) extends Table[Product](tag, "products") {
+    def productId = column[String]("productId", O.PrimaryKey)
+    def productName = column[String]("productName")
+    def productPrice = column[Double]("productPrice")
+    def productDetail = column[String]("productDetail")
+    def categoryId = column[String]("productCategoryId")
+    def * = (productId, productName, productPrice, productDetail) <> (Product.tupled,Product.unapply)
+  }
+}
