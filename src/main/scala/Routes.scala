b644c2af866137341a4b6a4369d624f33a011002
Eric <wan@informatique.univ-paris-diderot.fr>
2020-10-22 22:23:24 +0200

    split Routes.scala into Routes.scala, UserRoutes.scala and Product.scala

--- src/main/scala/Routes.scala
+++ src/main/scala/Routes.scala
@@ -1,16 +1,31 @@
 package poca
 
-import scala.concurrent.{ExecutionContext, Future}
-import akka.http.scaladsl.server.Directives.{complete, concat, formFieldMap, get, path, post}
+import akka.http.scaladsl.model.{
+  ContentTypes,
+  HttpEntity,
+  HttpResponse,
+  StatusCodes
+}
+import akka.http.scaladsl.server.Directives.{
+  complete,
+  concat,
+  formFieldMap,
+  get,
+  path,
+  post
+}
 import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpResponse, StatusCodes}
 import com.typesafe.scalalogging.LazyLogging
-import TwirlMarshaller._
 import play.twirl.api.HtmlFormat
+import scala.concurrent.{ExecutionContext, Future}
+import TwirlMarshaller._
 
+class Routes(users: Users, products: Products)
+    extends UserRoutes
+    with ProductRoutes {
 
-class Routes(users: Users, products: Products) extends LazyLogging {
-  implicit val executionContext: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
+  override implicit val executionContext: ExecutionContext =
+    scala.concurrent.ExecutionContext.Implicits.global
 
   def getHello(): HttpEntity.Strict = {
     logger.info("I got a request to greet.")
@@ -20,173 +35,46 @@ class Routes(users: Users, products: Products) extends LazyLogging {
     )
   }
 
-  def getSignIn: HtmlFormat.Appendable = {
-    logger.info("I got a request for signin.")
-    html.signin()
-  }
-
-  def getSignup(): HtmlFormat.Appendable = {
-    logger.info("I got a request for signup.")
-    html.signup()
-  }
-
-  def register(fields: Map[String, String]): Future[HttpResponse] = {
-    logger.info("I got a request to register.")
-
-    fields.get("username") match {
-      case Some(username) => {
-        val userCreation: Future[Unit] = users.createUser(username = username)
-
-        userCreation.map(_ => {
-          HttpResponse(
-            StatusCodes.OK,
-            entity = s"Welcome '$username'! You've just been registered to our great marketplace.",
-          )
-        }).recover({
-          case exc: UserAlreadyExistsException => {
-            HttpResponse(
-              StatusCodes.OK,
-              entity = s"The username '$username' is already taken. Please choose another username.",
-            )
-          }
-        })
-      }
-      case None => {
-        Future(
-          HttpResponse(
-            StatusCodes.BadRequest,
-            entity = "Field 'username' not found."
-          )
-        )
-      }
-    }
-  }
-
-  def addProduct(fields: Map[String, String]): Future[HttpResponse] = {
-    logger.info("I got a request to add a Product.")
-
-    var category: Option[Category] = None
-    fields.get("categoryName") match {
-      case Some(categoryName) => {
-	    category = Some(
-	      new Category(
-	  	    categoryId=None,
-	   	    categoryName=categoryName
-	      )
-	    )
-	  }
-	  case None => {
-		category = None
-	  }
-	}
-
-	val productCreation: Future[Unit] = products.createProduct(
-	  productName=fields.get("name").orNull,
-	  productPrice=fields.get("price").map(f => f.toDouble).get,
-	  productDetail=fields.get("detail").orNull,
-	  category=category
-	)
-    productCreation.map(_ => {
-      HttpResponse(
-        StatusCodes.OK,
-        entity = "Product successfully added to the marketplace."
-	  )
-    }).recover({
-      case exc: CategoryDoesntExistsException => {
-        HttpResponse(
-          StatusCodes.OK,
-          entity = "Cannot insert product, there is not this category."
-        )
-      }
-      case exc: IncorrectPriceException => {
-        HttpResponse(
-          StatusCodes.OK,
-          entity = "Cannot insert product with a price < 0"
-        )
-      }
-    })
-  }
-
-  def getUsers(): Future[HtmlFormat.Appendable] = {
-    logger.info("I got a request to get user list.")
-
-    val userSeqFuture: Future[Seq[User]] = users.getAllUsers
-
-    userSeqFuture.map(userSeq => html.users(userSeq))
-  }
-
-  def getProducts(): Future[HtmlFormat.Appendable] = {
-    logger.info("I got a request to get product list.")
-
-    val productSeqFuture: Future[Seq[Product]] = products.getAllProducts
-
-    productSeqFuture.map(productSeq => html.products(productSeq))
-  }
-
-  def buyProduct(fields: Map[String, String]): Future[HtmlFormat.Appendable] = {
-    val productId = fields.get("id").orNull
-    logger.info(s"I got a request to buy the product $productId");
-
-    val productFuture: Future[Product] = Future {
-      Product(
-        productId = fields.get("id").orNull,
-        productName = fields.get("name").orNull,
-        productPrice = fields.get("price").map(f => f.toDouble).get,
-        productDetail = fields.get("detail").orNull,
-        categoryId = None
-      )
-    }
-
-    Future(
-      HttpResponse(
-        StatusCodes.OK,
-        entity = s"Thank you ! you bought '$productId'"
-      )
-    )
-
-    productFuture.map(product => html.purchase(product))
-  }
-
   val routes: Route =
     concat(
       path("hello") {
         get {
-          complete(getHello)
+          complete(getHello())
         }
       },
       path("signin") {
         get {
-          complete(getSignIn)
+          complete(super[UserRoutes].getSignIn(users))
         }
       },
       path("signup") {
         get {
-          complete(getSignup)
+          complete(super[UserRoutes].getSignup(users))
         }
       },
       path("register") {
         (post & formFieldMap) { fields =>
-          complete(register(fields))
+          complete(super[UserRoutes].register(users, fields))
         }
       },
       path("users") {
         get {
-          complete(getUsers)
+          complete(super[UserRoutes].getUsers(users))
         }
       },
       path("products") {
         get {
-          complete(getProducts)
+          complete(super[ProductRoutes].getProducts(products))
         }
       },
       path("products") {
         (post & formFieldMap) { fields =>
-          complete(addProduct(fields))
+          complete(super[ProductRoutes].addProduct(products, fields))
         }
       },
       path("purchase") {
         (post & formFieldMap) { fields =>
-          complete(buyProduct(fields))
+          complete(super[ProductRoutes].buyProduct(products, fields))
         }
       }
     )
