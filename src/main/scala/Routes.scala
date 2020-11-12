2e5747fa986ac3fb8b49cf648773af2c5743e960
Joackim Brigout Cavaud <joackim.brigout@gmail.com>
2020-11-12 15:35:28 +0100

    configure auth with session

--- src/main/scala/Routes.scala
+++ src/main/scala/Routes.scala
@@ -3,9 +3,9 @@ package poca
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpMessage, HttpResponse, StatusCodes}
 import akka.http.scaladsl.server.Directives.{authenticateBasic, complete, concat, formFieldMap, get, path, post}
-import akka.http.scaladsl.server.directives.Credentials
+import akka.http.scaladsl.server.directives.{Credentials, OnSuccessMagnet}
 import akka.http.scaladsl.model.headers.BasicHttpCredentials
-import akka.http.scaladsl.server.{Route, StandardRoute}
+import akka.http.scaladsl.server.{AuthorizationFailedRejection, Route, StandardRoute}
 import com.softwaremill.session._
 
 import scala.concurrent.{Await, ExecutionContext, Future}
@@ -15,7 +15,7 @@ import auth.PocaSession
 import com.softwaremill.session.CsrfDirectives.{randomTokenCsrfProtection, setNewCsrfToken}
 import com.softwaremill.session.CsrfOptions.checkHeader
 import com.softwaremill.session.SessionDirectives.{invalidateSession, requiredSession, setSession}
-import com.softwaremill.session.SessionOptions.{refreshable, usingCookies}
+import com.softwaremill.session.SessionOptions.{oneOff, refreshable, usingCookies, usingHeaders}
 
 import scala.concurrent.duration.{Duration, MILLISECONDS}
 
@@ -25,6 +25,7 @@ class Routes(users: Users, products: Products, carts: Carts)
 
   override implicit val executionContext: ExecutionContext =
     scala.concurrent.ExecutionContext.Implicits.global
+
 
   val sessionConfig = SessionConfig.default(
     "p4NuwW0gCMGuwIVY1475AyIaZGVpFlMAmdtuErxa0Bjn6RDmidwZdv8ufohX3Z6L")
@@ -46,26 +47,25 @@ class Routes(users: Users, products: Products, carts: Carts)
     )
   }
 
-  def auth(fields: Map[String,String]) ={
-    val username = fields.get("username").orNull
-    val password = fields.get("password").orNull
-    val exist: Future[Option[User]] = users.getUserByUsername(username)
-    exist.map(user => {
-      if (user.isDefined) {
-        val validCredentials = BasicHttpCredentials(username, password)
-        redirect("/products", Found)
-      } else {
-        redirect("/signin", Found)
-      }
-    })
-  }
-
   //à modifier
-  def myUserPassAuthenticator(credentials: Credentials): Option[String] =
+  def myUserPassAuthenticator(credentials: Credentials) = {
     credentials match {
       case p @ Credentials.Provided(id) if p.verify("p4ssw0rd") => Some(id)
       case _ => None
     }
+  }
+
+  def checkCredentials(credentials: BasicHttpCredentials) ={
+    val existingUserFuture = users.getUserByUsername(credentials.username)
+    existingUserFuture.map{
+      user => {
+        if(user.isDefined && user.last.userPassword == credentials.password)
+          user
+        else
+          None
+      }
+    }
+  }
 
   val routes: Route =
     concat(
@@ -86,17 +86,17 @@ class Routes(users: Users, products: Products, carts: Carts)
           complete(super[UserRoutes].getSignIn(users))
         }
       },
-      randomTokenCsrfProtection(checkHeader) {
-        path("auth") {
-          logger.info(s"Access path 'auth'")
-          post {
-            entity(as[String]) { body =>
-              logger.info(s"Logging in $body")
-
-              mySetSession(PocaSession(body)) {
-                setNewCsrfToken(checkHeader) { ctx =>
-                  ctx.complete("ok")
-                }
+      path("auth") {
+        (post & formFieldMap) { fields =>
+          val username = fields.get("login").last
+          val password = fields.get("password").last
+          val credentials = (BasicHttpCredentials(username, password))
+          onSuccess(checkCredentials(credentials)) {
+            case None => reject(AuthorizationFailedRejection)
+            case Some(user) => {
+              val session = PocaSession(username.last.toString)
+              setSession(oneOff, usingHeaders, session) {
+                redirect("products", Found)
               }
             }
           }
@@ -118,25 +118,19 @@ class Routes(users: Users, products: Products, carts: Carts)
         }
       },
       path("products") {
-        authenticateBasic(realm = "secure site", myUserPassAuthenticator) { userName =>
-          (post & formFieldMap) { fields =>
-            complete(super[ProductRoutes].addProduct(products, fields))
-          }
+        (post & formFieldMap) { fields =>
+          complete(super[ProductRoutes].addProduct(products, fields))
         }
       },
       path("products") {
-        authenticateBasic(realm = "secure site", myUserPassAuthenticator) { userName =>
-          get {
-            complete(super[ProductRoutes].getProducts(products))
-          }
+        get {
+          complete(super[ProductRoutes].getProducts(products))
         }
       },
       path("products") {
-        authenticateBasic(realm = "secure site", myUserPassAuthenticator) { userName =>
           (post & formFieldMap) { fields =>
             complete(super[ProductRoutes].addProduct(products, fields))
           }
-        }
       },
       path("purchase") {
         (post & formFieldMap) { fields =>
